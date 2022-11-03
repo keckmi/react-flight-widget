251dd1b19d335f7bf3bed6d64f5c71c401c1f9df
m.keck <michael-s-keck@web.de>
2022-11-03 15:07:57 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const PORT = 8000
+const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+
+app.get('/flights', (req, res) => {
+    console.log(axios)
+    const options = {
+        url: `${process.env.URL}?page-size=6`,
+        headers: {
+            accept: 'application/json',
+            'X-Cassandra-Token': process.env.TOKEN
+        }
+    }
+
+    axios.request(options).then(response => {
+        console.log(response.data)
+        res.json(response.data)
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+app.listen(PORT, () => console.log('running on port' + PORT))
